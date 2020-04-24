9f4e09ea0a5867e88551765ed6d96d600c6bddba
Carlos Lozano <carlosmlozanos@gmail.com>
2020-04-24 11:05:20 +0200

    step 42 - Versioning RESTful Services - Header and Content Negotiation

--- src/main/java/com/example/versioning/PersonVersoningController.java
+++ src/main/java/com/example/versioning/PersonVersoningController.java
@@ -2,17 +2,68 @@ package com.example.versioning;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+/**
+ * In this Controller resume the ways to versioning a bean from lower to higher used style
+ * @author Carlos Lozano
+ *
+ */
 
 @RestController
 public class PersonVersoningController {
-
-	@GetMapping("v1/person")
-	public PersonV1 personV1() {
+	
+	/**
+	 * Parameter versioning
+	 * @return
+	 */
+	@GetMapping(value="/person", params="version=1")
+	public PersonV1 personParamsV1() {
 		return new PersonV1("Bob");
 	}
 	
-	@GetMapping("v2/person")
-	public PersonV2 personV2() {
+	/**
+	 * Parameter versioning
+	 * @return
+	 */
+	@GetMapping(value="/person", params="version=2")
+	public PersonV2 personParamsV2() {
 		return new PersonV2(new Name("Bob", "Charlie"));
 	}
+	
+	/**
+	 * Header versioning
+	 * @return
+	 */
+	@GetMapping(value="/person", headers="x-api-version=1")
+	public PersonV1 personHeadersV1() {
+		return new PersonV1("Bob");
+	}
+	
+	/**
+	 * Header versioning
+	 * @return
+	 */
+	@GetMapping(value="/person", headers="x-api-version=2")
+	public PersonV2 personHeadersV2() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+	
+	/**
+	 * Media Type versioning
+	 * @return
+	 */
+	@GetMapping(value="/person", produces="application/vnd.company.app-v1+json")
+	public PersonV1 personProducesV1() {
+		return new PersonV1("Bob");
+	}
+	
+	/**
+	 * Media Type versioning
+	 * @return
+	 */
+	@GetMapping(value="/person", produces="application/vnd.company.app-v2+json")
+	public PersonV2 personProducesV2() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+	
+	
 }
